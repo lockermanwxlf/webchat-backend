95fdb627c84321d7b6f37ec0af40bd03cb288b80
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-06-13 21:13:55 -0700

    Register ApplicationDbContext in services

--- Webchat.AuthorizationServer/Program.cs
+++ Webchat.AuthorizationServer/Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Webchat.AuthorizationServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register ApplicationDbContext with Cosmos provider.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Get Cosmos account endpoint from environment.
+    var endpoint = builder.Environment.IsProduction() ?
+        Environment.GetEnvironmentVariable("CosmosEndpoint")! :
+        builder.Configuration.GetValue<string>("CosmosEndpoint")!;
+
+    // Get Cosmos primary key from environment.
+    var primaryKey = builder.Environment.IsProduction() ?
+        Environment.GetEnvironmentVariable("CosmosPrimaryKey")! :
+        builder.Configuration.GetValue<string>("CosmosPrimaryKey")!;
+    
+    // Register cosmos provider.
+    options.UseCosmos(accountEndpoint: endpoint, accountKey: primaryKey, databaseName: "AuthorizationDB");
+});
 
 var app = builder.Build();
 
