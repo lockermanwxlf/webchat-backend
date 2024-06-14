f44aae00e628c24035bf6d68efc06b11acf0f674
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-06-13 21:08:08 -0700

    Create schema for identity users

---
+++ Webchat.AuthorizationServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Webchat.AuthorizationServer.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, int>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationRole>()
+                .Property(p => p.ConcurrencyStamp)
+                .IsETagConcurrency();
+            builder.Entity<ApplicationUser>()
+                .Property(p => p.ConcurrencyStamp)
+                .IsETagConcurrency();
+        }
+    }
+}
---
+++ Webchat.AuthorizationServer/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Webchat.AuthorizationServer.Data
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+}
