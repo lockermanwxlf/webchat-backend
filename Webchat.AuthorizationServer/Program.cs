b270e3af8107559d566ce6cf81f1bc3398e1ac8a
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2024-06-13 22:11:28 -0700

    Add OpenIddict services

--- Webchat.AuthorizationServer/Program.cs
+++ Webchat.AuthorizationServer/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Validation.AspNetCore;
+using Webchat.AuthorizationServer;
 using Webchat.AuthorizationServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,11 +23,58 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     
     // Register cosmos provider.
     options.UseCosmos(accountEndpoint: endpoint, accountKey: primaryKey, databaseName: "AuthorizationDB");
+
+    // Use OpenIddict
+    options.UseOpenIddict();
 });
 
 // Set up identity
 builder.Services.AddIdentityCore<ApplicationUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add OpenIddict
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+            .UseDbContext<ApplicationDbContext>();
+    })
+    .AddServer(options =>
+    {
+        // Set endpoint URIs.
+        options.SetTokenEndpointUris("auth/token");
+        options.SetIntrospectionEndpointUris("auth/introspect");
+
+        // Set allowed flows
+        options.AllowPasswordFlow();
+        options.AllowRefreshTokenFlow();
+
+        // Set keys
+        var encryptionKey = builder.Environment.IsProduction() ?
+            Environment.GetEnvironmentVariable("EncryptionKey")! :
+            builder.Configuration.GetValue<string>("EncryptionKey")!;
+        options.AddEncryptionKey(new SymmetricSecurityKey(
+            Convert.FromBase64String(encryptionKey)));
+        // The development certificate should suffice for Azure Container Apps, but
+        // technically I'm supposed to make a self-signed certificate and use that instead.
+        options.AddDevelopmentSigningCertificate();
+
+        // Register ASP.NET Core host and enable passthrough for endpoints.
+        options.UseAspNetCore()
+            .EnableTokenEndpointPassthrough();
+
+    })
+    // Add local validation as this container will host endpoints for account info.
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+
+        options.UseAspNetCore();
+    });
+
+// Add authentication and authorization
+builder.Services.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -33,12 +84,17 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
     ForwardedHeaders = ForwardedHeaders.XForwardedProto
 });
 
+// Ensure db exists and OpenIddict applications and scopes are registed.
 using (var scope = app.Services.CreateAsyncScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     await context.Database.EnsureCreatedAsync();
+    await Startup.CreateApplicationsAsync(scope);
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
